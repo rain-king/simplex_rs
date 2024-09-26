1d932cdae3ab24ed15e84f3a9414f41c5df20a56
Gustavo <43526958+rain-king@users.noreply.github.com>
2024-09-26 16:35:48 -0700

    changed nalgebra to ndarrays

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,32 @@
-mod matrix_io;
-use matrix_io::read_vector;
-use std::io::stdin;
-use std::io::{self, BufRead}; // needed to read empty lines without storing them
+#![allow(warnings)]
 
-extern crate nalgebra as na;
-use na::{DMatrix, RowDVector};
+use ndarray::Array2 as array;
+
+mod ndarray_io;
+use ndarray_io as io;
+
+mod simplex;
+use simplex::max_simplex;
+
+use std::io::stdin;
+use std::io::BufRead; // needed to read empty lines without storing them
+
 
 fn main() {
-	println!("This is a program that solves only the maximize");
+	println!("This is a program that solves the maximization of");
 	println!("Z = cx subject to Ax <= b with b >= 0.");
 	println!();
 
-	let mut row =
-		read_vector("Enter the c vector values separated by spaces, then press return again.");
+	let mut c = io::read_row("Enter the c vector values separated by spaces, then press return again.");
 	stdin().lock().lines().next(); // read empty line
-	let c = RowDVector::from_vec(row);
 	
-	println!("Write the A matrix row by row, with values separated by spaces,");
-	println!("then press Return again.");
-	let mut a_rows = Vec::new();
-	loop {
-		row = read_vector("");
-		if row.is_empty() {
-			break;
-		}
-		a_rows.push(RowDVector::from_vec(row));
-	}
+	let mut a_matrix = io::read_matrix("Write the A matrix row by row, with values separated by spaces, then press Return again.");
 
-	let a_matrix = DMatrix::from_rows(&a_rows);
+	let mut b = io::read_column("Enter the b vector values separated by spaces.");
+		
+	// println!("{c}");
+	// println!("{a_matrix}");
+	// println!("{b}");
 
-	println!("{c}");
-	println!("{a_matrix}");
-
+	max_simplex(c, a_matrix, b);
 }
