636cc8e818f7cb7fe85a2c66218240e59c7e40f5
Gustavo <43526958+rain-king@users.noreply.github.com>
2024-09-25 21:50:05 -0700

    reads A matrix and c vector

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,35 @@
+mod matrix_io;
+use matrix_io::read_vector;
 use std::io::stdin;
+use std::io::{self, BufRead}; // needed to read empty lines without storing them
 
-fn parse_float_vector(linea: &str) -> Vec<f64> {
-	let mut vector: Vec<f64> = Vec::new();
-	for numero in linea.split_whitespace() {
-		vector.push(numero.parse().unwrap());
-	}
-	vector
-}
-
-fn read_matrix() -> Result<Vec<Vec<f64>>, String> {
-	let mut matriz: Vec<Vec<f64>> = Vec::new();
-	let mut linea: String = String::new();
-
-	println!("Introduzca los valores de las filas una por una");
-	println!("separados por espacios, y al terminar pulse enter:");
-	
-	let mut i: usize = 0;
-	loop {
-		stdin().read_line(&mut linea).unwrap();
-		linea = linea.trim().to_string();
-		if linea.is_empty() {
-			break;
-		}
-		matriz.push(parse_float_vector(&linea));
-		linea.clear();
-		if i > 0 {
-			if matriz[i].len() != matriz[i-1].len() {
-				return Err(format!("Las filas de la matriz deben ser de igual tamaño."));
-			}
-		}
-		i += 1;
-	}
-	Ok(matriz)
-}
+extern crate nalgebra as na;
+use na::{DMatrix, RowDVector};
 
 fn main() {
-	let matriz: Vec<Vec<f64>> = read_matrix().unwrap();
+	println!("This is a program that solves only the maximize");
+	println!("Z = cx subject to Ax <= b with b >= 0.");
+	println!();
 
-	println!("La matriz es: ");
-	for vector in matriz {
-		for valor in vector {
-			print!("{valor}\t");
+	let mut row =
+		read_vector("Enter the c vector values separated by spaces, then press return again.");
+	stdin().lock().lines().next(); // read empty line
+	let c = RowDVector::from_vec(row);
+	
+	println!("Write the A matrix row by row, with values separated by spaces,");
+	println!("then press Return again.");
+	let mut a_rows = Vec::new();
+	loop {
+		row = read_vector("");
+		if row.is_empty() {
+			break;
 		}
-		println!();
+		a_rows.push(RowDVector::from_vec(row));
 	}
+
+	let a_matrix = DMatrix::from_rows(&a_rows);
+
+	println!("{c}");
+	println!("{a_matrix}");
+
 }
