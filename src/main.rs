7113913cf1760ca00b7713b085976c17f256d39c
Gustavo <43526958+rain-king@users.noreply.github.com>
2024-09-27 07:44:01 -0700

    added examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-#![allow(warnings)]
-
-use ndarray::Array2 as array;
+// #![allow(warnings)]
 
 mod ndarray_io;
 use ndarray_io as io;
@@ -17,16 +15,13 @@ fn main() {
 	println!("Z = cx subject to Ax <= b with b >= 0.");
 	println!();
 
-	let mut c = io::read_row("Enter the c vector values separated by spaces, then press return again.");
+	let c = io::read_row("Enter the c vector values separated by spaces, then press return again.");
 	stdin().lock().lines().next(); // read empty line
 	
-	let mut a_matrix = io::read_matrix("Write the A matrix row by row, with values separated by spaces, then press Return again.");
+	let a_matrix = io::read_matrix("Write the A matrix row by row, with values separated by spaces, then press Return again.");
 
-	let mut b = io::read_column("Enter the b vector values separated by spaces.");
+	let b = io::read_column("Enter the b vector values separated by spaces.");
 		
-	// println!("{c}");
-	// println!("{a_matrix}");
-	// println!("{b}");
 
 	max_simplex(c, a_matrix, b);
 }
--- src/simplex.rs
+++ src/simplex.rs
@@ -1,40 +1,77 @@
 use core::f64;
-use std::f32::EPSILON;
+
+use crate::ndarray_io;
+use ndarray_io::pretty_print_array2;
 
 use ndarray as nd;
-use nd::{Axis, concatenate, Array2 as matrix, Array1 as vector};
+use nd::{Axis, concatenate, Array2 as matrix, Array1 as vector, s};
 
 pub fn max_simplex(c: matrix<f64>, a_matrix: matrix<f64>, b: matrix<f64>)
-	// -> Vec<i64>
-{
+	-> Vec<(usize, usize)>
+{	// tableau initialization
 	let slacks: matrix<f64> = matrix::eye(a_matrix.nrows());
 
 	assert_eq!(a_matrix.nrows(), slacks.nrows(), "Row counts do not match");
 
-	let mut tableu_restrictions: matrix<f64> = concatenate![Axis(1), a_matrix, slacks];
+	let mut tableau_restrictions: matrix<f64> = concatenate![Axis(1), a_matrix, slacks];
 
-	tableu_restrictions = concatenate![Axis(1), tableu_restrictions, b];
+	tableau_restrictions = concatenate![Axis(1), tableau_restrictions, b];
 
 	// println!("{:?}", c);
 	// println!("{:?}", matrix::<f64>::zeros((1,1)));
 
-	let z_row = concatenate![Axis(1), -c.clone(), matrix::<f64>::zeros((1,tableu_restrictions.ncols()-c.ncols()))];
+	let z_row = concatenate![Axis(1), -c.clone(), matrix::<f64>::zeros((1,tableau_restrictions.ncols()-c.ncols()))];
 
-	let mut tableu = concatenate![Axis(0), z_row, tableu_restrictions];
+	let mut tableau = concatenate![Axis(0), z_row, tableau_restrictions];
 
-	println!("{:?}", tableu);
+	println!("The initial tableau is:");
+	pretty_print_array2(&tableau);
+	println!();
+
+	let basis = iterations(&mut tableau);
+
+	println!("The final tableau is:");
+	pretty_print_array2(&tableau);
+
+	basis
 }
 
-// THIS CODE SUPPOSES THE TABLEU IS IN BASIC FORM
-fn iteration(tableu: matrix<f64>) -> Vec<(usize, usize)> {
-	let mut basis = Vec::new();
+// THIS CODE SUPPOSES THE TABLEAU IS IN BASIC FORM
+fn iterations(tableau: &mut matrix<f64>) -> Vec<(usize, usize)> {
+	// create initial basis from basic form
+	let mut basis = initialize_basis(tableau.to_owned());
 
-	for j in 0..(tableu.ncols()-1) {
-		let col = tableu.column(j);
-		for i in 1..tableu.nrows() {
-			let col_sum: f64 = col.iter().sum();
-			if (col_sum == 1.0) && ((tableu[(i,j)] - 1.0).abs() < f64::EPSILON)  {
-				basis.push((i, j));
+	println!("Initial basis:\n{:?}", basis);
+
+	while tableau.row(0).slice(s![..-1]).into_iter().any(|&x| x < 0.0) {
+		let (pivot_row_index, pivot_column_index) = pivot(tableau);
+		// println!("The pivot indexes are: {pivot_row_index} {pivot_column_index}:");
+		for i in 0..basis.len() {
+			// variable with pivot column enters, variable with pivot row exits
+			if basis[i].0 == pivot_row_index {
+				// println!("{:?}", basis);
+				basis[i] = (pivot_row_index, pivot_column_index);
+			}
+		}
+		println!("The current basis indexes are\n{:?}", basis);
+		// pretty_print_array2(&tableau);
+		println!("");
+	}
+
+	basis
+}
+
+fn initialize_basis(tableau: matrix<f64>) -> Vec<(usize, usize)> {
+	let mut basis = Vec::new();
+	for j in 0..(tableau.ncols()-1) { // avoid right hand side
+		let col = tableau.column(j).slice(s![1..]).to_owned();
+		let has_only_one_1 = col.iter().filter(|&&x| x == 1.0).count() == 1;
+		let everything_else_is_0 = col.iter().filter(|&&x| x == 0.0).count() == col.len() - 1;
+		if has_only_one_1 && everything_else_is_0 {
+			for i in 1..tableau.nrows() {
+				if tableau[(i,j)] == 1.0 {
+					basis.push((i,j));
+				}
 			}
 		}
 	}
@@ -42,11 +79,43 @@ fn iteration(tableu: matrix<f64>) -> Vec<(usize, usize)> {
 	basis
 }
 
-fn pivot(tableu: matrix<f64>) -> (usize, usize) {
-	let pivot_column = argmin(tableu.row(0).to_owned());
-	let pivot_row = 0 as usize;
+fn pivot(tableau: &mut matrix<f64>) -> (usize, usize) {
+	let (pivot_row_index, pivot_column_index) = pivot_indexes(tableau);
 
-	(pivot_column, pivot_row)
+	let normalization_scalar = tableau[(pivot_row_index, pivot_column_index)].to_owned();
+	tableau.row_mut(pivot_row_index).map_inplace(|x| *x /= normalization_scalar);
+
+	let pivot_row = tableau.row(pivot_row_index).to_owned();
+	for mut row in tableau.rows_mut().into_iter() {
+		if row != pivot_row {
+			let pivot_value = row[pivot_column_index];
+			row.zip_mut_with(&pivot_row, |r, p| *r -= p*pivot_value);
+		}
+	}
+
+	(pivot_row_index, pivot_column_index)
+}
+
+fn pivot_indexes(tableau: &mut matrix<f64>) -> (usize, usize) {
+	let pivot_column_index = argmin(tableau.row(0).slice(s![..-1]).to_owned());
+	// let quotients: Vec<f64> = Vec::new();
+	let mut pivot_row_index = 0 as usize;
+	let mut minimum = f64::INFINITY;
+	for (i, pivot_value) in tableau.column(pivot_column_index).into_iter().enumerate() {
+		// minimize quotients
+		if i > 0 {
+			let right_hand_side_value = tableau[(i, tableau.ncols() - 1)];
+			if *pivot_value > 0.0 {
+				let quotient = right_hand_side_value/pivot_value;
+				if quotient < minimum {
+					minimum = quotient;
+					pivot_row_index = i;
+				}
+			}
+		}
+	}
+
+	(pivot_row_index, pivot_column_index)
 }
 
 fn argmin(arr: vector<f64>) -> usize {
